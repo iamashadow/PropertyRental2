c5774f8978a6e8390f115bcc61504814e955c55e
Md. Sifatullah <sifatullah35-617@diu.edu.bd>
2024-05-16 01:27:56 +0600

    login

--- lib/Pages/Login_Page/login_page_controller.dart
+++ lib/Pages/Login_Page/login_page_controller.dart
@@ -1,10 +1,19 @@
+import 'dart:convert';
 import 'dart:math';
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
+import 'package:property_rental_2/Pages/Home_Page/home_page.dart';
+import 'package:property_rental_2/Pages/Login_Page/model/login_rp.dart';
+import 'package:property_rental_2/Utils/constant.dart';
+import 'package:property_rental_2/Utils/secure_storage.dart';
 import '../LandLord_Sector/Page1/Land_Lord_Profile_Information_Page/land_lord_profile_information_page.dart';
+import 'package:http/http.dart' as http;
 
-class LoginPageControllerClass extends GetxController{
-
+class LoginPageControllerClass extends GetxController {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  var isLoading = false.obs;
+  UserData userData = UserData();
 
   String generateRandomNumber() {
     final Random random = Random();
@@ -19,24 +28,52 @@ class LoginPageControllerClass extends GetxController{
 
   final GlobalKey<FormState> formKey = GlobalKey<FormState>();
 
-  void submitForm(){
-    if(formKey.currentState!.validate()){
+  void submitForm() {
+    if (formKey.currentState!.validate()) {
       Get.to(LandLordProfileInformationPage());
     }
   }
 
-  String? validateEmail (value){
-    if(value!.isEmpty){
+  String? validateEmail(value) {
+    if (value!.isEmpty) {
       return "Please enter an email";
     }
 
     RegExp emailRegExp = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
-    if(!emailRegExp.hasMatch(value)){
+    if (!emailRegExp.hasMatch(value)) {
       return "Please enter a valid email";
     }
     return null;
-
   }
 
+  Future<void> loginRequest() async {
+    try {
+      isLoading.value = true;
+      final response = await http.post(
+        Uri.parse('$baseurl/landlord/login'),
+        headers: <String, String>{
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode({
+          'email': emailController.text,
+          'password': passwordController.text,
+        }),
+      );
 
+      if (response.statusCode == 200) {
+        final LoginResponse loginResponse =
+            loginResponseFromJson(response.body);
+        userData = loginResponse.data!;
+        await SecureData.writeSecureData(key: 'userData', value: userData);
+        isLoading.value = false;
+        Get.to(() => HomePage());
+      } else {
+        isLoading.value = false;
+        printError(info: response.body);
+      }
+    } catch (e) {
+      printError(info: e.toString());
+      isLoading.value = false;
+    }
+  }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,7 @@
 import FlutterMacOS
 import Foundation
 
+import flutter_secure_storage_macos
 import package_info_plus
 import path_provider_foundation
 import url_launcher_macos
@@ -12,6 +13,7 @@ import video_player_avfoundation
 import wakelock_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
